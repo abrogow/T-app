de639502375a76199b6f4442427b7ffe3bd7768d
abrogow <abrogow@gmail.com>
2018-11-24 21:21:49 +0100

    writer ans finder classes

---
+++ src/model/finder/Finder.java
@@ -0,0 +1,16 @@
+package model.finder;
+
+import java.util.ArrayList;
+
+public class Finder {
+
+	private ArrayList<Long> resultPositions;
+
+	public ArrayList<Long> findRecords() {
+
+		resultPositions = new ArrayList<Long>();
+
+		// TODO: wyszukiwanie rekordów z pliku
+		return resultPositions;
+	}
+}
---
+++ src/model/writer/UniprotWriter.java
@@ -0,0 +1,32 @@
+package model.writer;
+
+import model.FastaRecord;
+
+public class UniprotWriter extends Writer {
+
+	private String descLine;
+	private String sequence;
+
+	@Override
+	public String getSequenceLine(FastaRecord record) {
+
+		sequence = null;
+		if (record != null) {
+			sequence = record.getSequence();
+		}
+		return sequence;
+	}
+
+	@Override
+	public String getDescLine(FastaRecord record) {
+		// TODO Auto-generated method stub
+		descLine = null;
+		if (record != null) {
+			descLine = ">sp|" + record.getIdentyfier() + "|" + record.getEnteryName() + " " + record.getProteinName()
+					+ " OS=" + record.getOrganismName() + " GN=" + record.getGenemName() + " PE="
+					+ record.getProteinExistence() + " SV=" + record.getSequenceVersion();
+		}
+		return descLine;
+	}
+
+}
